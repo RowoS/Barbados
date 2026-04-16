fd481341f4864c004f6f5a20e44bef190eb961dc
Jamesnel Chavez <officialketchupped@gmail.com>
2026-04-16 11:30:01 +0800

    feat: Add Vendor Catalog Page for Menu Management

---
+++ app/(authenticated)/vendor/catalog/page.tsx
@@ -0,0 +1,21 @@
+import CatalogPage from "@/features/order/components/page";
+import { SidebarInset, SidebarProvider } from "@/shared/ui/sidebar";
+import VendorNavBar from "@/features/vendor/components/shared/VendorNavBar";
+import VendorSidebar from "@/features/vendor/components/shared/VendorSideBar";
+import { Package } from "lucide-react";
+
+export default function VendorCatalog() {
+  return (
+    <SidebarProvider>
+      <VendorSidebar />
+
+      <SidebarInset>
+        <VendorNavBar />
+          <main className="pt-25">
+            <CatalogPage/>
+          </main>
+      </SidebarInset>
+
+    </SidebarProvider>
+  )
+}
