5c0083cab520e6fc30b64b18b4ee90fb10710bda
Jamesnel Chavez <officialketchupped@gmail.com>
2026-04-20 05:55:31 +0800

    refactor: move store insert function from hook to lib
    - move the store insert function into vendor-actions.ts

--- features/vendor/libs/vendor-actions.ts
+++ features/vendor/libs/vendor-actions.ts
@@ -1,6 +1,24 @@
+"use client";
 import { createClient } from "@/lib/supabase/client";
 
-export async function LogOutUser () {
+
+export async function createStore(payload: {
+    id: string;
+    store_name: string;
+    store_description: string | null;
+    store_logo: string | null;
+    store_coordinates: string | null;
+    phone_numbers: string[];
+    address: string;
+    opening_time: string | null;
+    closing_time: string | null;
+    delivery_options: string;
+}) {
     const supabase = createClient();
-    return supabase.auth.signOut();
+
+    const { error } = await supabase.from('stores').insert(payload);
+
+    if (error) {
+        throw error;
+    }
 }
