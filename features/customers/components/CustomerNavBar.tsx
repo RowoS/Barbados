7e6dffaa6f0179b56e1b675bbc391e7c0d0d68c6
Jamesnel Chavez <officialketchupped@gmail.com>
2026-04-20 11:48:39 +0800

    feat: add Customer Navbar component

---
+++ features/customers/components/CustomerNavBar.tsx
@@ -0,0 +1,74 @@
+"use client";
+
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "@/shared/components/dropdown-menu";
+
+import { Bell, User, UtensilsCrossed, LogOut } from "lucide-react";
+import { Button } from "@/shared/ui/button";
+import CustomerProfileOverlay from "@/features/profiles/components/overlays/CustomerProfileModal"
+import { useCustomerNavBar } from "../hooks/CustomerNavBarLogic";
+import { CustomerNavBarProps } from "../types/types";
+
+export default function CustomerNavBar({ onProfileOpenChange }: CustomerNavBarProps) {
+  const { goToProfile,handleLogout,openProfile,setOpenProfile, isLoading} = useCustomerNavBar();
+
+   const handleSetOpenProfile = (value: boolean) => {
+        setOpenProfile(value);
+        onProfileOpenChange?.(value);
+    };
+
+  return (
+    <header className="bg-accent-black fixed top-0 left-0 right-0 z-50">
+      <div className="w-full px-6 sm:px-6 lg:px-8">
+        <div className="flex items-center justify-between h-16">
+          <div className="flex items-center gap-2">
+            <div className="w-10 h-10 bg-[var(--accent-blue)] rounded-lg flex items-center justify-center">
+              <UtensilsCrossed className="w-6 h-6 text-white" />
+            </div>
+            <span className="text-xl font-bold text-white">
+              Buybites
+            </span>
+          </div>
+
+          <div className="flex items-center gap-3">
+            
+            <DropdownMenu>
+              <DropdownMenuTrigger asChild>
+                <Button className="flex items-center gap-2 bg-accent-orange hover:bg-hover-orange text-white px-5 py-2 rounded-lg">
+                  <User className="w-4 h-4" />
+                  <span>Profile</span>
+                </Button>
+              </DropdownMenuTrigger>
+
+              <DropdownMenuContent align="end" className="w-48">
+                <DropdownMenuItem onClick={() => handleSetOpenProfile(true)}>
+                  User Profile
+                </DropdownMenuItem>
+
+                <DropdownMenuItem
+                  onClick={handleLogout}
+                  className="text-red-500"
+                  disabled={isLoading}
+                >
+                  <LogOut className="w-4 h-4 mr-2" />
+                  {isLoading ? "Logging out..." : "Logout"}
+                </DropdownMenuItem>
+              </DropdownMenuContent>
+            </DropdownMenu>
+
+            <Button className="flex items-center gap-2 bg-accent-orange hover:bg-hover-orange text-black px-5 py-2 rounded-lg">
+              <Bell className="w-4 h-4" />
+            </Button>
+
+          </div>
+        </div>
+
+        {openProfile && (<CustomerProfileOverlay onClose={() => handleSetOpenProfile(false)} />)}
+      </div>
+    </header>
+  );
+}
