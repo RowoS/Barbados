84a35c8c48c9e5a786d73327c42d575ace13c169
Jamesnel Chavez <officialketchupped@gmail.com>
2026-06-03 22:26:58 +0800

    refactor: Update Layout and Color Scheme of Vendor Review Page
    - Review Distribution, Average Rating, and and Total Review Count are now in one section
    - Filter and Searching are now in the same container
    - Update the overall Color Scheme and Margins of the Page

--- features/reviews/components/ReviewPageWrapper.tsx
+++ features/reviews/components/ReviewPageWrapper.tsx
@@ -5,13 +5,14 @@ import VendorSidebar from "@/features/vendor/components/shared/VendorSideBar";
 
 export default function ReviewPageWrapper() {
     return (
-    <SidebarProvider className="bg-orange-50">
+    <SidebarProvider style={{ background: "#FFF8F5" }}>
       <VendorSidebar />
 
-      <SidebarInset className="bg-orange-50">
+      <SidebarInset style={{ background: "#FFF8F5" }}>
         <VendorNavBar />
         <main className="min-h-screen p-6">
-            <div className="max-w-9xl mx-auto">
+            <div className="max-w-9xl mx-auto"
+            style={{ marginRight: "8rem" }}>
                 <VendorReviewPage />
             </div>
         </main>
